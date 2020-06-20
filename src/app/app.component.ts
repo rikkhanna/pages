69b05d1f351e511f1de96531bad8707ba9cd249a
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-06-20 18:39:01 +0530

    attribute, structural and custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: "At the Beach",
+      imageUrl: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80"
+    },
+    {
+      title: "At the Forest",
+      imageUrl: "https://images.unsplash.com/photo-1440688807730-73e4e2169fb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+    },
+    {
+      title: "At the City",
+      imageUrl: "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=813&q=80"
+    },
+    {
+      title: "At the Snow",
+      imageUrl: "https://images.unsplash.com/photo-1516820208784-270b250306e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=282&q=80"
+    }
+  ];
+
+  checkWindowIndex(index: number){
+    return Math.abs(this.currentPage - index) < 2;
+  }
 }
